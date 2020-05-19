9e6fb43c6f42649774a57cd76878778ebe71d181
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-05-18 21:35:38 -0500

    docker script fix

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 APP_NAME="covid-app"
 APP_PORT=90
-docker rmi $(docker images | grep $APP_NAME)
-docker build -t $APP_NAME .
+docker rmi $(docker images | grep $APP_NAME) || true && \
+docker build -t $APP_NAME . && \
 docker run -d -p $APP_PORT:80 $APP_NAME
